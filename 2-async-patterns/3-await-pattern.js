51bd975499ee29644e320e2c3e4774a81d6dd45f
KRIS-25 <krisrego.sdcce@vvm.edu.in>
2023-03-15 11:05:34 +0530

    commited node tutorial

---
+++ 2-async-patterns/3-await-pattern.js
@@ -0,0 +1,25 @@
+const { readFile } = require("fs");
+
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (error, data) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+const start = async () => {
+  try {
+    const first = await getText("../content/first.txt");
+    const second = await getText("../content/second.txt");
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
